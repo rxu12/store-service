ccfff212f1f85dc0a1180eb360cbe5514f56a974
Rui Xu <rxu@officeworks.com.au>
2020-06-08 20:08:53 +1000

    mock store api

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID = "2"397860,
+
+namespace myMicroservice.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StoreController : ControllerBase
+    {
+        private readonly ILogger<StoreController> _logger;
+
+        public StoreController(ILogger<StoreController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("{storeId}")]
+        public Store Get(string storeId)
+        {
+            _logger.LogInformation(storeId);
+            return new Store
+            {
+                id = "2",
+                partnerId = "2",
+                name = "2",
+                status = "2",
+                openingDate = DateTime.Now,
+                closingDate = DateTime.Now.AddDays(1),
+                generalNotification = "2",
+                openingNotification = "2",
+                priorityNotificationText = "2"
+            };
+        }
+    }
+}
---
+++ Store.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace myMicroservice
+{
+    public class Store
+    {
+        public string id { get; set; }
+
+        public string partnerId { get; set; }
+
+        public string name { get; set; }
+
+        public string status { get; set; } // Should use Enum
+
+        public DateTime openingDate { get; set; }
+
+        public DateTime closingDate { get; set; }
+
+        public string generalNotification { get; set; }
+
+        public string openingNotification { get; set; }
+
+        public string priorityNotificationText { get; set; }
+
+        //public Address address { get; set; }
+
+        //public GeoLocation geolocation { get; set; }
+     
+        //public StoreHours openingHours { get; set; }
+
+        //public StoreHours exceptionHours { get; set; }
+
+        //public StoreService services { get; set; }
+    }
+}
